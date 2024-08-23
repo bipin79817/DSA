ec6bec76a882891b4276d538f47028e13c8c419d
Bipin Yadav <140512135+bipin79817@users.noreply.github.com>
2024-08-23 23:08:39 +0530

    Time: 16 ms (71.07%), Space: 18.6 MB (56.90%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -14,18 +14,18 @@ public:
                 low--;
                 high++;
             }
-            string palindrome = s.substr( low + 1, high - low - 1);
+            string palindrome = s.substr( low + 1, high-low-1);
             if (palindrome.length() > lps.length()) {
                 lps = palindrome;
             }
 
             // Even-length palindrome
-            low = i, high = i + 1;
+            low = i, high = i+1 ;
             while (low >= 0 && high < n && s[low] == s[high]) {
                 low--;
                 high++;
             }
-            palindrome = s.substr(low + 1, high - low - 1);
+            palindrome = s.substr(low + 1, high-low-1);
             if (palindrome.length() > lps.length()) {
                 lps = palindrome;
             }
