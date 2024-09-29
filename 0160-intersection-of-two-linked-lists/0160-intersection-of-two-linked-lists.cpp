4368d35eaaa036c046c67b00fd0958bb43ae47a2
Bipin Yadav <140512135+bipin79817@users.noreply.github.com>
2024-09-30 02:14:30 +0530

    Time: 36 ms (66.82%), Space: 17.4 MB (24.60%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -11,17 +11,53 @@ public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
         ListNode* temp1=headA;
         ListNode* temp2=headB;
-        map<ListNode*, int> mp;
-        while(temp1!=NULL){
-            mp[temp1]=1;
+//         map<ListNode*, int> mp;
+//         while(temp1!=NULL){
+//             mp[temp1]=1;
+//             temp1=temp1->next;
+//         }
+        
+//         while(temp2!=NULL){
+//         if(mp.find(temp2)!=mp.end()){
+//             return temp2;
+//         }
+//         temp2=temp2->next;
+//         }
+//         return nullptr;
+        
+        //METHOD 2
+        int cnt1=1,cnt2=1;
+        
+        if (!headA || !headB) return nullptr;
+        
+        while(temp1->next!=NULL){
+            cnt1++;
             temp1=temp1->next;
         }
-        
-        while(temp2!=NULL){
-        if(mp.find(temp2)!=mp.end()){
-            return temp2;
+        while(temp2->next!=nullptr){
+            cnt2++;
+            temp2=temp2->next;
         }
-        temp2=temp2->next;
+        
+        
+        temp1=headA;
+        temp2=headB;
+        
+        if(cnt1>cnt2){
+            for(int i=0; i<cnt1-cnt2; i++){
+                temp1=temp1->next;
+            }
+        }else{
+            for(int i=0; i<cnt2-cnt1; i++){
+                temp2=temp2->next;
+            }
+        }
+        while(temp1!=nullptr || temp2!=nullptr){
+            if(temp1==temp2){
+                return temp2;
+            }
+            temp1=temp1->next;
+            temp2=temp2->next;
         }
         return nullptr;
     }
