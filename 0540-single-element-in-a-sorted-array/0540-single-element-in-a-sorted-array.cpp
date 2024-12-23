ba9bdb352db96aacc129d857d30bc386a6c254d5
Bipin Yadav <140512135+bipin79817@users.noreply.github.com>
2024-12-24 01:30:08 +0530

    Time: 0 ms (100.00%), Space: 26.3 MB (17.97%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,26 +1,56 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        //BRUTE FORCE APPROACH
+    //BRUTE FORCE APPROACH
+        
+      //   if(nums.size()==1){
+      //       return nums[0];
+      //   }
+      //   for(int i=0;i<nums.size(); i++){
+      //       if(i==0){
+      //           if(nums[i]!=nums[i+1]){
+      //               return nums[i];
+      //           }
+      //       }else if(i==nums.size()-1){
+      //           if(nums[i]!=nums[i-1]){
+      //               return nums[i];
+      //           }
+      //       }else{
+      //           if(nums[i]!=nums[i+1] && nums[i]!=nums[i-1]){
+      //               return nums[i];
+      //           }
+      //       }
+      //   }
+      // return 0;
+        
+    // OPTIMAL SOLUTION
         
         if(nums.size()==1){
             return nums[0];
         }
-        for(int i=0;i<nums.size(); i++){
-            if(i==0){
-                if(nums[i]!=nums[i+1]){
-                    return nums[i];
-                }
-            }else if(i==nums.size()-1){
-                if(nums[i]!=nums[i-1]){
-                    return nums[i];
-                }
+        
+        if(nums[0]!=nums[1]){
+            return nums[0];
+        }
+        
+        if(nums[nums.size()-1]!=nums[nums.size()-2]){
+            return nums[nums.size()-1];
+        }
+        
+        int left=1;
+        int right=nums.size()-2;
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            if(nums[mid]!=nums[mid+1] && nums[mid]!=nums[mid-1]){
+                return nums[mid];
+            }
+            
+            if(mid%2==0 && nums[mid]==nums[mid+1] || mid%2==1 && nums[mid]==nums[mid-1]){
+                left=mid+1;
             }else{
-                if(nums[i]!=nums[i+1] && nums[i]!=nums[i-1]){
-                    return nums[i];
-                }
+                right=mid-1;
             }
         }
-      return 0;
+        return 0;
     }
 };
