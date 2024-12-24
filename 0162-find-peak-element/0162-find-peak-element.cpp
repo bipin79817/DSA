0deeab987ea03b8dbdb9a4a3444b7982789a42e5
Bipin Yadav <140512135+bipin79817@users.noreply.github.com>
2024-12-25 01:52:58 +0530

    Time: 0 ms (100.00%), Space: 12.4 MB (29.67%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -1,6 +1,42 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
+        //ONE APPROACH
+        
+//         int left=1;
+//         int right=nums.size()-2;
+        
+//         if(nums.size()==1){
+//             return 0;
+//         }
+        
+//         if(nums[0]>nums[1]){
+//             return 0;
+//         }
+        
+//         if(nums[nums.size()-1]>nums[nums.size()-2]){
+//             return nums.size()-1;
+//         }
+        
+//         while(left<=right){
+//             int mid=left+(right-left)/2;
+            
+//             if(nums[mid]>nums[mid+1] && nums[mid]>nums[mid-1]){
+//                 return mid;
+//             }
+//             if(nums[left]>nums[left+1] && nums[left]>nums[left-1]){
+//                 return left;
+//             }else if(nums[right]>nums[right+1] && nums[right]>nums[right-1]){
+//                 return right;
+//             }else{
+//                 left=left+1;
+//                 right=right-1;
+//             }
+//         }
+//         return 1;
+        
+        // SECOND METHOD
+        
         int left=1;
         int right=nums.size()-2;
         
@@ -21,14 +57,10 @@ public:
             
             if(nums[mid]>nums[mid+1] && nums[mid]>nums[mid-1]){
                 return mid;
-            }
-            if(nums[left]>nums[left+1] && nums[left]>nums[left-1]){
-                return left;
-            }else if(nums[right]>nums[right+1] && nums[right]>nums[right-1]){
-                return right;
+            }else if(nums[mid]>nums[mid-1]){
+                left=mid+1;
             }else{
-                left=left+1;
-                right=right-1;
+                right=mid-1;
             }
         }
         return 1;
